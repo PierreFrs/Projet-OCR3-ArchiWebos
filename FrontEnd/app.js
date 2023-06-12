b31fe66664e1d6492488d1a3c5e0637871198601
Pierre <pierrefraisse90@gmail.com>
2023-06-12 12:19:15 +0200

    feat : projects filters

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -8,12 +8,39 @@ const fetchWorks = async () => {
   return data;
 };
 
+// Fonction créant les boutons de manière dynamique
+const filtersDOM = document.querySelector(".filters-container");
+
+const displayButtons = async () => {
+  const list = await fetchWorks();
+  const buttons = [
+    "Tous",
+    ...new Set(list.map((project) => project.categoryId)),
+  ];
+  filtersDOM.innerHTML = buttons
+    .map((button) => {
+      return `<button class='filter-btn' data-id="${button}">${
+        button === 1
+          ? "Objets"
+          : button === 2
+          ? "Appartements"
+          : button === 3
+          ? "Hotels & restaurants"
+          : "Tous"
+      }</button>`;
+    })
+    .join("");
+};
+
+displayButtons();
+
 // Fonction d'affichage dynamique des projets
 const displayGallery = (list) => {
   const projectsList = list
     .map((project) => {
       const { id, title, imageUrl, categoryId } = project;
-      return `<figure id=${id} category-id=${categoryId}>
+
+      return `<figure id=${id} data-id=${categoryId}>
             <img src=${imageUrl} alt=${title} />
             <figcaption>${title}</figcaption>
           </figure>`;
@@ -29,7 +56,22 @@ const onLoad = async () => {
 };
 onLoad();
 
-// afficher la liste des travaux en dynamique (.map())
+// Event listeners des boutons filtres
+
+filtersDOM.addEventListener("click", async (e) => {
+  const el = e.target;
+  const list = await fetchWorks();
+  if (el.classList.contains("filter-btn")) {
+    if (el.dataset.id === "Tous") {
+      filteredProjects = list;
+    } else {
+      filteredProjects = list.filter((project) => {
+        return project.categoryId === parseInt(el.dataset.id);
+      });
+    }
+    displayGallery(filteredProjects);
+  }
+});
 
 // Page de connexion : integration
 
