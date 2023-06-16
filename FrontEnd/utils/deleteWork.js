e71c3663053fd64d7090b1a4ab7b436fb42e6a41
Pierre <pierrefraisse90@gmail.com>
2023-06-16 09:33:37 +0200

    feat : delete all first steps

--- FrontEnd/utils/deleteWork.js
+++ FrontEnd/utils/deleteWork.js
@@ -7,14 +7,29 @@ import fetchWorks from "./fetchWorks.js";
 // selectionner la poubelle pour chaque photo (e.target...)
 
 const modaleGallery = document.querySelector(".modale-gallery");
+const deleteAll = document.querySelector(".gallery-del");
 
-modaleGallery.addEventListener("click", (e) => {
-  // Check if the clicked element is a trashcan icon
-  if (e.target.matches(".fa-trash-can")) {
-    e.preventDefault();
-    handleTrashcanClick(e);
+const authToken = window.localStorage.getItem("token");
+
+// envoyer requete fetch DELETE au serveur avec le token pour un element
+const handleTrashcanClick = (e) => {
+  const trashIcon = e.target;
+  if (trashIcon.classList.contains("fa-trash-can")) {
+    const modaleGalleryItem = trashIcon.closest(".modale-gallery-item");
+    const itemId = modaleGalleryItem.id;
+    console.log("Delete item with ID:", itemId);
+
+    // Call your delete request function here with the item ID and authToken
+    deleteItem(itemId, authToken);
   }
-});
+};
+
+// // mettre à jour la gallery à la réceptionn de la réponse du serveur
+const updateGalleries = async () => {
+  const list = await fetchWorks();
+  await displayGallery(list);
+  await modaleGalleryDisplay(list, modaleGallery);
+};
 
 // Créer la requete fetch DELETE
 const deleteItem = async (itemId, authToken) => {
@@ -45,23 +60,45 @@ const deleteItem = async (itemId, authToken) => {
   }
 };
 
-// envoyer requete fetch DELETE au serveur avec le token
-const handleTrashcanClick = (e) => {
-  const authToken = window.localStorage.getItem("token");
-  const trashIcon = e.target;
-  if (trashIcon.classList.contains("fa-trash-can")) {
-    const modaleGalleryItem = trashIcon.closest(".modale-gallery-item");
-    const itemId = modaleGalleryItem.id;
-    console.log("Delete item with ID:", itemId);
+// const handleDeleteAllClick = (e) => {
+//   e.preventDefault();
 
-    // Call your delete request function here with the item ID and authToken
-    deleteItem(itemId, authToken);
+//   deleteItems();
+// };
+
+// const deleteItems = async () => {
+//   const url = `http://localhost:5678/api/works/`;
+//   try {
+//     const response = await fetch(url, {
+//       method: "DELETE",
+//       headers: {
+//         Authorization: `Bearer ${authToken}`,
+//       },
+//     });
+
+//     if (response.ok) {
+//       console.log("Deletion successful");
+//       updateGalleries();
+//       return true; // Deletion successful
+//     } else if (!authToken) {
+//       console.log("Please provide a valid authentification token");
+//       return false; // Deletion failed cause of invalid or missing token
+//     } else {
+//       console.log("Deletion failed");
+//       return false; // Deletion failed
+//     }
+//   } catch (error) {
+//     console.error("Error deleting item", error);
+//     return false; // Request or network error
+//   }
+// };
+
+modaleGallery.addEventListener("click", (e) => {
+  // Check if the clicked element is a trashcan icon
+  if (e.target.matches(".fa-trash-can")) {
+    e.preventDefault();
+    handleTrashcanClick(e);
   }
-};
+});
 
-// // mettre à jour la gallery à la réceptionn de la réponse du serveur
-const updateGalleries = async () => {
-  const list = await fetchWorks();
-  await displayGallery(list);
-  await modaleGalleryDisplay(list, modaleGallery);
-};
+// deleteAll.addEventListener("click", handleDeleteAllClick);
